326fcc9e65257830d8d96403020ffa5ded0c4a68
cmckain14 <cmckain14@users.noreply.github.com>
2013-11-22 23:15:21 -0800

    Added a lot of good stuff
    1. Table now stores results, will soon save them to file
    2. Can now ping 'minecrafttownyislands.com'
    3. Compares default IP to actual one 
    4. Now can set which function (table or status) to begin with
    5. More general improvements

--- PingProgram.lua
+++ PingProgram.lua
@@ -1,11 +1,36 @@
 --Functions--
+function table()
+	if firstattheend == "table" then
+		os.execute( "cls" )
+		end
+	INS = "Internet Status: "
+	ROS = "Router Status: "
+	DNS = "DNS Status: "
+	if MTIA == "Yes" then
+		MTI = "Towny Islands Status: "
+		end
+	--AIT = "Alt. Internet Status: "
+	r = {si, dsr, sd, mti} --Table of network results, without the Alt. Internet (add 'asi')
+	print(ROS..r[2])
+	print(INS..r[1])
+	print(DNS..r[3])
+	if MTIA == "Yes" then
+		print(MTI..r[4])
+		end
+	--print(AIT..r[4])
+	if firstattheend == "status" then
+		endMessage()
+		end
+	end
 function note()
 	if note == nil then
 		print("Note: Ignore the above information, the status of your connection will be shown below. Please press 'enter' to proceed.")
 		end	
 	end
 function status()
-	os.execute( "cls" )
+	if firstattheend == "status" then
+		os.execute( "cls" )
+		end
 	if sr == nil and si == nil then 
 		sr = "[UNKNOWN]"
 		si = "[UNKNOWN]"
@@ -18,11 +43,16 @@ function status()
 		if si == nil then
 			error("Warning: Variable 'si' is nil.")
 			end
-		end	
-	print("The status of the connection to the main router is " ..sr..".")
+		end
+	if diff == 1 then
+		ds = 3
+		end
+	if diff == nil then
+		print("The status of the connection to the main router is " ..sr..".")
+		end
 	print("The status of the connection to the internet is " ..si..".")
 	if asi ~= nil then
-		print("The status of the connection to a router is " ..asi..".")	
+		print("The status of the connection to a router, another than the main one ("..arip..") is " ..asi..".")	
 		end
 	if ds == 0 then
 		print("The internet is fully operational!")
@@ -34,13 +64,20 @@ function status()
 		print("The internet is down, please try again later.")
 		end
 	if ds == 3 then
-		print("You are connected to the internet but not to the main router.")
+		print("You are connected to the internet but not to the main router.                   ("..arip.." vs. "..rip..")") --Leave the space there 
 		end
 	if ds == 4 then
 		print("The router you are connected to (not the main one) is not connected to the internet.")
 		end
-	print("Press 'enter' when you are finished reading the above information.")
-	io.stdin:read'*l'
+	if firstattheend == "table" then	
+		endMessage()
+		end
+	end
+function getIP() -- Do not change anything here
+	someRandomIP = "192.168.1.122" 
+	someRandomPort = "3102" 
+	mySocket = socket.udp() 
+	mySocket:setpeername(someRandomIP,someRandomPort) 
 	end
 function debugging()
 	debugit = "off" --On or off for debug mode.
@@ -66,8 +103,7 @@ function debugging()
 			end
 		if ir ~= nil then
 			print(ir)
-			end
-		print("Thank you for using my Network Status tool written in Lua!")	
+			end	
 		end	
 	if debugit == "off" then 
 		if debugfun == "on" then
@@ -75,19 +111,34 @@ function debugging()
 			end
 		if debugfun == "off" then
 			print("Debug off.")
-			end
-		print("Thank you for using my Network Status tool written in Lua!")	
+			end	
 		end	
 	if debugit == "NOTA" then
-		print("Thank you for using my Network Status tool written in Lua!")
 		end
-	end		
---Network Variables--	
-rip = "192.168.0.1" --Change this to your router's IP address	
+	end
+function endMessage()
+	if MTIA == "Yes" then
+		if mti == "[OK]" then
+			print("and the best news is...you were able to connect to the Towny Islands website!")
+			end
+		end	
+	print("Press 'enter' when you are finished reading the above information.")
+	io.stdin:read'*l'
+	end
+--Variables-- --Set these! :)	
+rip = "192.168.1.1" --Change this to your router's IP address	
 website = "google.com" --Change this to the site you would like to ping
+firstattheend = "table" --Which function goes first? The table or status?
+MTIA = "No" --Should there be an attempt to access the Towny Islands website? (Please say 'Yes')
 --Start of code--
 require("socket")
+require("table")
 ip = socket.dns.toip(website)
+getIP()
+arip = mySocket:getsockname() 
+if rip ~= arip then
+	diff = 1
+	end
 if ip == nil then
 	error("No IP address found")
 	end	
@@ -106,6 +157,10 @@ if mr == 1 then
 	os.execute( "cls" )
 	sr = "[BAD]"
 	si = "[BAD]"
+	end
+dsr = sr	
+if diff == 1 then
+	dsr = "[DIFFERENT IP, BUT OK]" 
 	end
 print("The status of the connection to the router is " ..sr..".")
 if si == "[READY]" then
@@ -126,17 +181,16 @@ if si == "[READY]" then
 		end
 	if si == "[OK]" and sd == "[OK]" then
 		ds = 0
-		status()
 		end
 	if si == "[BAD]" then
 		ds = 2
 		end
 	if sd == "[BAD]" then
 		ds = 1
-		status()
 		end
+	--asi = "[NOT_NEEDED]"	
 	end	
-if sr == "[BAD]" then
+if si == "[BAD]" then
 	air = os.execute("ping google.com -n 1") --Alternate Internet Testing
 	if air == 0 then
 		asi = "[OK]"
@@ -147,11 +201,20 @@ if sr == "[BAD]" then
 	if asi == "[OK]" then
 		ds = 3
 		si = "[OK]"
-		status()
 		end
 	if asi == "[BAD]" then
 		ds = 4
-		status()
 		end	
+	end
+if MTIA == "Yes" then	
+	mtiwt = os.execute("ping minecrafttownyislands.com -n 1")
+	if mtiwt == 0 then
+		mti = "[OK]"
+		end
+	if mtiwt == 1 then
+		mti = "[BAD]"
+		end
 	end	
 debugging()	
+table()
+status()
